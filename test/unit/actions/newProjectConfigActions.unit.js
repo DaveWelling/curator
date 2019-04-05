646964c76155b3fa450bcbde45443165c2f78321
david.welling <david.welling@sstid.com>
2019-04-05 17:48:47 -0400

    fuckin watermelon is killin me.

--- test/unit/actions/newProjectConfigActions.unit.js
+++ test/unit/actions/newProjectConfigActions.unit.js
@@ -1,37 +1,40 @@
 import expect, { spyOn, createSpy, restoreSpies } from 'expect';
 import { Database } from '@nozbe/watermelondb';
 import LokiJSAdapter from '@nozbe/watermelondb/adapters/lokijs';
+//import 'jsdom-worker';
 
 import schema from '../../../src/model/schema';
 import { Item, ItemUi, Project } from '../../../src/model/Item';
 import { newLoadProjectConfig } from '../../../src/actions/projectConfigActions';
 
-const adapter = new LokiJSAdapter({
-    dbName: 'Test',
-    schema
-});
-
-// Then, make a Watermelon database from it!
-const database = new Database({
-    adapter,
-    modelClasses: [Item, ItemUi, Project],
-    actionsEnabled: true
-});
-
-const testDb = new Database({
-    adapter: database.adapter.testClone(),
-    schema,
-    modelClasses: [Item, ItemUi, Project],
-    actionsEnabled: true
-});
-
 describe.only('newLoadProjectConfig', function() {
-    beforeEach(function(){
-        return testDb.action(async () => database.collections.get('project').create(project => {
+    let adapter, database, testDb;
+    beforeEach(function() {
+        adapter = new LokiJSAdapter({
+            dbName: 'Test',
+            schema
+        });
+
+        // Then, make a Watermelon database from it!
+        database = new Database({
+            adapter,
+            modelClasses: [Item, ItemUi, Project],
+            actionsEnabled: true
+        });
+
+        debugger;
+        testDb = new Database({
+            adapter: database.adapter.testClone(),
+            schema,
+            modelClasses: [Item, ItemUi, Project],
+            actionsEnabled: true
+        });
+        return testDb.action(async () => testDb.collections.get('project').create(project => {
             project.title = 'test1';
         }));
     });
     it('gets an existing project', async function() {
+        debugger;
         const project = await newLoadProjectConfig(testDb);
         expect(project.title).toEqual('test1');
     });
